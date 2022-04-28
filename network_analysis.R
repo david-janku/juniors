4dbfa8b999d06c6e385c7408de4477c4bd9b0d3f
David Janku <davidjanku1@seznam.cz>
2022-04-28 16:12:18 +0200

    dopsane indikatory tematicke nezavislosti

--- network_analysis.R
+++ network_analysis.R
@@ -118,15 +118,15 @@ clustering <-
         isolates = c("NaN", "zero")
 )
 
-#vypočtení finálního skoru independence
-RII = ((1-supeig)+clustering)/2
+#vypočtení skoru independence z coauthorské síte
+network_independence = ((1-supeig)+clustering)/2
 
 #vypočtení tematickeho překryvu
 
 library(data.table)
 require(data.table)
 root.direct = here::here()
-text = fread(here::here(root.direct, 'Binter_Kvapilova_pubs.csv'))
+text = fread(here::here("data", "raw", 'Binter_Kvapilova_pubs.csv'))
 all.text = text$pubs_title
 corpus = tolower(all.text)
 
@@ -142,7 +142,7 @@ tokens = tokens_select(tokens, sw,  selection = "remove")
 names(tokens) = text$ID_core_pubs
 
 tokens.dfm = dfm(tokens, tolower = FALSE)
-dfm = dfm_trim(tokens.dfm, min_termfreq = 10, termfreq_type="count")
+dfm = dfm_trim(tokens.dfm, min_termfreq = 2, termfreq_type="count") #tady asi dává smysl dát tam nějaké nízké číslo, protože sposutu autorů nebude mít zas tolik publikací a pokud se i třeba jen 10 % svých publikací (což můžou být třeba jen  publikace jako u tohoto autora) odlišují od vedoucího, chcem to zachytit) 
 dim(dfm) 
 terms = colnames(dfm)
 singles = terms[nchar(terms)==1]
@@ -153,23 +153,23 @@ C = terms[grep("[0-9][0-9][A-Za-z]",terms,perl = T)]
 D = terms[grep("[A-Za-z][0-9][0-9]",terms,perl = T)] 
 dfm = dfm[,terms]
 dim(dfm) 
-save(dfm, file = here::here(root.direct,"bitner_dfm_min10_full_text.Rdata"))
+save(dfm, file = here::here("data", "derived","bitner_dfm_min5_full_text.Rdata"))
 
 library("topicmodels")
 library("ggplot2")
 library("scales")
 library("ldatuning")
-load(here::here(root.direct, "bitner_dfm_min10_full_text.Rdata"))
+load(here::here("data", "derived", "bitner_dfm_min5_full_text.Rdata"))
 set.seed(123)
 
-dfm <- dfm[which(rowSums(dfm) > 0),] #tady v tomto momentě mi zmizí asi 100 datapointů a nevím proč - vytvoří to ale komplikaci níže 
+dfm <- dfm[which(rowSums(dfm) > 0),] #v tomto momentě zmizí 10 datapointů protože mají nulovou hodnotu (tzn používaly pouze "stopwords? to je nějaké divné) 
 dim(dfm)
 dtm <- convert(dfm,to="topicmodels")
 dim(dtm)
 
 result <- FindTopicsNumber(
     dtm,
-    topics = c(5, 15, 50, 75, 100, 125, 150, 175),
+    topics = c(2, 5, 10, 15, 30),
     metrics = c("Griffiths2004", "CaoJuan2009", "Deveaud2014"),
     method = "Gibbs",
     control = list(seed = 77),
@@ -186,24 +186,99 @@ library("tm")
 dfm = dfm[which(rowSums(dfm) > 0),]
 dtm = convert(dfm,to="topicmodels")
 dim(dtm)
-lda.model = LDA(dtm,k = 15, control = list(seed = 123),alpha = 0.1, beta = 0.01 , verbose=1) 
-save(lda.model, file = here::here(root.direct, "bitner_lda_full_text_15_topics_min10.RDS"))
+lda.model = LDA(dtm,k = 5, control = list(seed = 123),alpha = 0.1, beta = 0.01 , verbose=1) 
+save(lda.model, file = here::here("data", "derived", "bitner_lda_full_text_5_topics_min5.RDS"))
 
 rowSums(lda.matrix)
 
 lda.matrix = posterior(lda.model,dfm)$topics
 dim(lda.matrix) 
 
-save(lda.matrix, file = here::here(root.direct, "bitner_topic_dist_full_text_15_topics_min10.Rdata"))
+save(lda.matrix, file = here::here("data", "derived", "bitner_topic_dist_full_text_5_topics_min5.Rdata"))
 library(data.table) 
 library(ggplot2)
 
 library(lsa)
-# data <- as.matrix(lda.matrix)
 data <- as.data.frame(t(lda.matrix))
 data <- as.matrix(data)
 
+
+##1. zpusob vypoctu indikatoru -> cosinova vzdalenost publikaci daneho autora od publikaci jeho vedouciho  
 cosine_matrix <- lsa::cosine(data)
+
+#zpracování souboru a merge do jedné tabulky
+data[,-1] %>% dim()
+
+data_test <- cosine_matrix %>% 
+    as.data.frame() %>% 
+    rownames_to_column("name") %>% 
+    as_tibble() %>%
+    inner_join(enframe(all.text, value = "title") %>% mutate(name = as.character(name)))
+
+text <- as.data.table(text)
+data_test <- as.data.table(data_test)
+
+data_test$name <- as.integer(data_test$name)
+
+finaldata <- merge.data.table(text, data_test, by.y = "name", by.x = "ID_core_pubs", all = TRUE) #tady páruju ty datasety
+
+
+# následující funkce se snaží 1) udělat průměr ze všech cosinů dané publikace se všemi publikacemi vedoucího 
+finaldata <- filter(finaldata, finaldata$author==1)
+
+finaldata$avg <- rowMeans(finaldata[ ,95:151]) #tady jsem si ale musel manualne najit poradi sloupců -> melo by to jit nějak vic automatizovane 
+
+# tady počítám průměr všech přechozích průměrů, což by měl být nejspíš ta finální metrika -> jedná se o průměrnou cosine similarity všech publikací daného autora od všech publikací jeho vedoucího (čím menší, tím více je autor nezávislý, čím větší tím méně je nezávislý) )
+
+topic_independence_cosine = mean(finaldata$avg, na.rm = TRUE)
+
+# vypocet celého indexu nezávislosti 
+
+RII_1 = ((1-supeig)+clustering+(1-topic_independence_cosine))/3
+
+##2. zpusob vypoctu indikatoru -> pocet temat ve kterých je autor jedinecne zastoupen
+
+topics <- c(0)
+topics <- as.data.table(topics)
+
+#tahle funkce říká: pokud v daném tématu nemá sledovaný výzkumník ani jednu publikaci s loadingem  více než 0,1, pak mi vytiskni "0", pokud má alespoň jednu, ale zároveň i jeho supervisors má alsepoň jednu, tak mi vytiskni "2", a pokud má alespoň jednu ale supervisor nemá žádnou, tak vytiskni "1". Takhle funkce bohužel není škálovatelná, takže se bude muset přepsat.   
+topics$topic_one <- if(sum(ifelse(data[1,1:20]>0.1, 1, 0))<1){print("0")
+} else if(sum(ifelse(data[1,21:77]>0.1, 1, 0))>1){print("2")
+} else {
+    print("1")
+}
+
+topics$topic_two <- if(sum(ifelse(data[2,1:20]>0.1, 1, 0))<1){print("0")
+} else if(sum(ifelse(data[2,21:77]>0.1, 1, 0))>1){print("2")
+} else {
+    print("1")
+}
+
+topics$topic_three <- if(sum(ifelse(data[3,1:20]>0.1, 1, 0))<1){print("0")
+} else if(sum(ifelse(data[3,21:77]>0.1, 1, 0))>1){print("2")
+} else {
+    print("1")
+}
+
+topics$topic_four<- if(sum(ifelse(data[4,1:20]>0.1, 1, 0))<1){print("0")
+} else if(sum(ifelse(data[4,21:77]>0.1, 1, 0))>1){print("2")
+} else {
+    print("1")
+}
+
+topics$topic_five <- if(sum(ifelse(data[5,1:20]>0.1, 1, 0))<1){print("0")
+} else if(sum(ifelse(data[5,21:77]>0.1, 1, 0))>1){print("2")
+} else {
+    print("1")
+}
+
+topics <- rm(topics$topics)
+
+topic_independence_categories = sum(ifelse(topics==1, 1, 0))/(sum(ifelse(topics==2, 1, 0)))+(sum(ifelse(topics==1, 1, 0)))
+
+#podle https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0202712#sec002 se topic independence categories násobí 2x  
+RII_2 = ((1-supeig)+clustering+topic_independence_categories*2)/3
+
 
 
 # radim kod
@@ -216,3 +291,7 @@ cosine_matrix <- lsa::cosine(data)
 #   distinct() %>% 
 #   ungroup()
 # AuthorGraph2 <- graph_from_edgelist(as.matrix(edgelist_df %>% select(-pub_id)), directed = FALSE)
+
+tolower(edgelist_df) #funkce k uprave jmen
+remove_punct = T,
+remove_symbols = F, remove_hyphens = F
