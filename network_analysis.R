16af0b4a54df11db55461e3e7e6f94025af626f8
David Janku <davidjanku1@seznam.cz>
2022-05-03 13:03:07 +0200

    network model edited and final calculations added

--- network_analysis.R
+++ network_analysis.R
@@ -5,22 +5,21 @@ install.packages("DiagrammeR")
 library(tidyverse)
 library(dplyr)
 library(igraph)
-library(DiagrammeR)
+library(targets)
+
 
 
 #this seems useful: https://www.jessesadler.com/post/network-analysis-with-r/
 #and this also seems useful: https://amirhosblog.wordpress.com/2016/09/29/co-authorship-analysis-in-r/
 #and this: https://eiko-fried.com/create-your-collaborator-network-in-r/
 
-#creating coauthorship network - this didnt work - below trying adapting code from: https://stackoverflow.com/questions/33540449/creating-an-edge-list-from-co-authorship-data
-
+#creating coauthorship network
 oneauth <- read.csv2(here::here("data", "raw", "Binter_pubs.csv"))
 
 split_authors <- strsplit(as.character(oneauth$list), ':')
 head(split_authors)
 
-dat <- rbind(split_authors)
-
+dat <- enframe(split_authors) %>% unnest(cols = "value") #co to dela> vem list a udelej y nej data frame /. defualtne to udela sloupce name a value. fce unnest pak veyme v2ci co jsou v listovym formatu a rozbali je a pokud je tam víc hodnot tak pro ně vytvoři nove řadky 
 
 # radim kod - vyhazuje mi to error se kterým nevím co dělat
  edgelist_df <- dat %>% group_by(name) %>% 
@@ -30,89 +29,55 @@ dat <- rbind(split_authors)
           to = pmax(value.x, value.y)) %>% 
    select(pub_id = name, from, to) %>% 
    distinct() %>% 
-   ungroup()
+   ungroup() %>% 
+     mutate(from = tolower(stringi::stri_trans_general(from, "latin-ascii")),
+            to = tolower(stringi::stri_trans_general(from, "latin-ascii"))) %>% 
+        mutate(from = str_replace(from, "(.*), (.?){1}.*", "\\1\\2"),
+               to = str_replace(to, "(.*), (.?){1}.*", "\\1\\2"))
+            
 
- # AuthorGraph2 <- graph_from_edgelist(as.matrix(edgelist_df %>% select(-pub_id)), directed = FALSE)
-
-tolower(edgelist_df) #funkce k uprave jmen
-remove_punct = T,
-remove_symbols = F, remove_hyphens = F
-
-
-
-#this is the kind of structure I need to run this code:
-# dat <- rbind(c("Miyazaki T.", "Akisawa A.", "Saha B.B.", "El-Sharkawy I.I.", "Chakraborty A."),
-  #           c("Saha B.B.", "Chakraborty A.", "Koyama S.", "Aristov Y.I.", NA),
-   #          c("Ali S.M.", "Chakraborty A.", NA, NA, NA))
-
-# loop through all rows of dat (all papers, I presume)
-#transformed.dat <- lapply(1:nrow(dat), function(row.num) {
-    
-#    row.el <- dat[row.num, ] # the row element that will be used in this loop
-    
-#    n.authors <- length(row.el[!is.na(row.el)])
-    
-    # creates a matrix with all possible combinations (play around with n.authors, to see what it does)
-#    pairings <- combn(n.authors, 2)
-    
-    # loop through all pairs and return a vector with one row and two columns
-#    res <- apply(pairings, 2, function(vec) {
-#        return(t(row.el[vec]))
-#    })
-    
-    # create a data.frame with names aut1 and aut2
-#   res <- data.frame(aut1 = res[1, ],
-#                      aut2 = res[2, ])
-                      
-    
-#    return(res)
-#})
-
-#final.dat <- data.table::rbindlist(transformed.dat)
-
-#to make it work, I would need to either transform the split authors list to data.frame, or I would need to find a way how to put NAs into the empty fields in the list) 
-
-#max.authors <- max(sapply(split_authors, length))
-
-#dat <- data.frame(aut1 = res[1, ],
-    #              aut2 = res[2, ])
-
-#creating coauthorship network - this worked! - adapted from: https://stackoverflow.com/questions/57487704/how-to-split-a-string-of-author-names-by-comma-into-a-data-frame-and-generate-an
-
-AuthorCombinations <- sapply(split_authors,function(x){combn(unlist(x),m = 2)})
-AuthorEdges <- rapply(AuthorCombinations,unlist)
-names(AuthorEdges) <- NULL
-
-AuthorEdges <- trimws(AuthorEdges)
+authorgraph <- graph_from_edgelist(as.matrix(edgelist_df %>% select(-pub_id)), directed = FALSE)
 
 
-print(AuthorEdges)
+# treti cast indikatoru: pocet publikaci daneho vyzkumnika bez vedouciho jako spoluautora deleno poctem vsech jeho publikaci
+ 
+n <- filter(oneauth, !str_detect(oneauth$list, "Klapilová, Kateřina"))
 
-AuthorGraph <- graph(AuthorEdges, directed = FALSE)
+rpubs = nrow(n)/nrow(oneauth)
 
+#alternative to creating coauthorship network - this worked but I didnt use it - adapted from: https://stackoverflow.com/questions/57487704/how-to-split-a-string-of-author-names-by-comma-into-a-data-frame-and-generate-an
 
+    #AuthorCombinations <- sapply(split_authors,function(x){combn(unlist(x),m = 2)})
+    #AuthorEdges <- rapply(AuthorCombinations,unlist)
+    #names(AuthorEdges) <- NULL
+
+    #AuthorEdges <- trimws(AuthorEdges)
+
+    #AuthorGraph <- graph(AuthorEdges, directed = FALSE)
 
 
 #plotting the graph
 par("mar")       #this was to prevent one error: https://stackoverflow.com/questions/23050928/error-in-plot-new-figure-margins-too-large-scatter-plot
 par(mar=c(1,1,1,1))
 
-plot(AuthorGraph)
+plot(authorgraph)
 
 
 #taking one subpart of the network and plotting it
-Excerpt <- induced_subgraph(AuthorGraph,c("Binter, Jakub", "Prossinger, Hermann"))
+Excerpt <- induced_subgraph(authorgraph,c("Binter, Jakub", "Prossinger, Hermann"))
 par("mar")
 par(mar=c(1,1,1,1))
 plot(Excerpt)
 
-##next steps: check whether the created network actually makes sense and is correct. Also - is it weighted graph or not?
 
-##calculating the metrics according to https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0202712#sec002)
+#alternative to creating coauthorship network - this didnt work - below trying adapting code from: https://stackoverflow.com/questions/33540449/creating-an-edge-list-from-co-authorship-data
+
+
+##next steps: check whether the created network actually makes sense and is correct. Also - is it weighted graph or not?
 
 #calculating eigenvector centrality using https://igraph.org/r/doc/eigen_centrality.html
 ecentr <- eigen_centrality(
-    AuthorGraph,
+    authorgraph,
     directed = FALSE,
     scale = TRUE,
     weights = NULL,
@@ -130,12 +95,12 @@ supeig <- sup$vector
 
 #calculating clustering coefficient of the former supervisor in the ego-network of the researcher: https://igraph.org/r/doc/transitivity.html
 
-list.vertex.attributes(AuthorGraph)   #from https://stackoverflow.com/questions/20209303/how-to-get-vertex-ids-back-from-graph
-which(V(AuthorGraph)$name == "KlapilovĂˇ, KateĹ™ina")
+list.vertex.attributes(authorgraph)   #from https://stackoverflow.com/questions/20209303/how-to-get-vertex-ids-back-from-graph
+which(V(authorgraph)$name == "KlapilovĂˇ, KateĹ™ina")
 
 clustering <-
     transitivity(
-        AuthorGraph,
+        authorgraph,
         type = c("local"),
         vids = 29,
         weights = NULL,
@@ -301,5 +266,5 @@ topics <- rm(topics$topics)
 topic_independence_categories = sum(ifelse(topics==1, 1, 0))/(sum(ifelse(topics==2, 1, 0)))+(sum(ifelse(topics==1, 1, 0)))
 
 #podle https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0202712#sec002 se topic independence categories násobí 2x  
-RII_2 = ((1-supeig)+clustering+topic_independence_categories*2)/3
+RII_2 = ((1-supeig)+clustering+rpubs+topic_independence_categories*2)/4
 
