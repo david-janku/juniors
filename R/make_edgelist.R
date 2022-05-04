92d02520b1b5dafead7909288883a0ceba044c10
hlageek <radim.hladik@fulbrightmail.org>
2022-05-04 11:06:24 +0200

    start targets

---
+++ R/make_edgelist.R
@@ -0,0 +1,14 @@
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#'
+#' @title
+#' @param one_author
+#' @return
+#' @author hlageek
+#' @export
+make_edgelist <- function(one_author) {
+
+  NULL
+
+}
