37ad1e4a4ff286cdefa7b3a31d893613d9046247
david-janku <davidjanku1@seznam.cz>
2023-04-14 17:08:15 +0200

    small changes

--- R/comp_ind_pubs.R
+++ R/comp_ind_pubs.R
@@ -8,10 +8,10 @@
 #' @return
 #' @author fatal: unable to access 'C:/Users/David Jank?/Documents/.config/git/config': Invalid argument
 #' @export
-comp_ind_pubs <- function(clustr) {
+comp_ind_pubs <- function(cluster) {
 
 
-independent_pubs <-  clustering %>% 
+independent_pubs <-  cluster %>% 
                         mutate(ind_pubs = purrr::pmap(.l = list(pub_table, sup_name),
                                    .f = function(first, second){
                                        calc_ind_pubs(one_author = first, sup_name = second)
